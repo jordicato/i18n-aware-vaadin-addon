da89f5e517d2dd3f3ba908ca4847cb5e85f77f8e
prodriguez@opnworks.com <prodriguez@opnworks.com@5d8f52dd-c860-2128-4ada-ecb7be3c41c2>
2013-01-19 18:28:48 +0000

    Fixing character encoding error

--- trunk/i18n-aware-appconverter/src/main/java/com/opnworks/vaadin/i18n/converter/I18NConverter.java
+++ trunk/i18n-aware-appconverter/src/main/java/com/opnworks/vaadin/i18n/converter/I18NConverter.java
@@ -62,9 +62,9 @@ import com.vaadin.data.util.filter.IsNull;
 
 /**
  * Class to get all vaadin widgets captions key
- * 
+ *
  * @author opnworks
- * 
+ *
  */
 public class I18NConverter {
 
@@ -90,29 +90,29 @@ public class I18NConverter {
 		String fullClassName;
 		int suffix;
 		int maxSuffixClass;
-			
+
 		public Tkey(String key, String value, String fullClassName, int suffix, int maxSuffixClass) {
 			this.key = key;
 			this.fullClassName = fullClassName;
 			this.value = value;
 			this.suffix = suffix;
 			this.maxSuffixClass = maxSuffixClass;
-		}		
+		}
 		public void setSuffixClass(int suffix){
 			this.maxSuffixClass = suffix;
 		}
 	}
-		
+
 	class TStringValue
 	{
 		String id;
-		String value;		
+		String value;
 		public TStringValue(String id, String value) {
 			this.id = id;
 			this.value = value;
-		}		
+		}
 	}
-	
+
 	private String javaFileName;
 	private String javaFileFullClassName;
 	private String[] validMethods = {"setCaption","setDescription", "addComponent", "showNotification", "setDescriptionMessage"};
@@ -120,33 +120,33 @@ public class I18NConverter {
 	private List<Tkey> generalListKey = new ArrayList<Tkey>();
 	private List<TStringValue> listStringValue = new ArrayList<TStringValue>();
 	private List<ImportDeclaration> lidtarget;
-	
+
 	public I18NConverter() {
 
 	}
-	
-	
-	private boolean isInKeyList(String key, List<Tkey> list){		
+
+
+	private boolean isInKeyList(String key, List<Tkey> list){
 		//listKey.contains(k);
 		for (Tkey k : list){
 			if (k.key.equals(key)){
 				return true;
 			}
 		}
-		return false;		
+		return false;
 	}
 
 	//Determina si un objeto Tkey contiene un value
-	private boolean isValueInKeyList(String value, List<Tkey> list){		
+	private boolean isValueInKeyList(String value, List<Tkey> list){
 		//listKey.contains(k);
 		for (Tkey k : list){
 			if (k.value.equals(value)){
 				return true;
 			}
 		}
-		return false;		
+		return false;
 	}
-	
+
 	//Determina si un objeto Tkey contiene una determinada key y value
 	private int valueAndKeyInList(String key, String value, List<Tkey> list){
 		int v = 0;
@@ -158,11 +158,11 @@ public class I18NConverter {
 		} else {
 			v = 3;
 		}
-		
+
 		return v;
-		
+
 	}
-	
+
 	private void sumSuffix(String key, int count, List<Tkey> list){
 		for (Tkey k : list){
 			if (k.key.equals(key)){
@@ -170,7 +170,7 @@ public class I18NConverter {
 			}
 		}
 	}
-	
+
 	//Actualizar el estado de los sufijos para cada Tkey
 	private void updateSuffixMax(String key, List<Tkey> list){
 		if (!list.isEmpty()){
@@ -182,20 +182,20 @@ public class I18NConverter {
 			}
 			sumSuffix(key, count, list);
 			count = 0;
-		}		
+		}
 	}
-	
+
 	/*private int existSuffix(String caption){
 		int pos = 0;
 		int num = 0;
 		if (caption.contains("_")){
-			pos = caption.indexOf("_"); 
+			pos = caption.indexOf("_");
 			num = Integer.parseInt(caption.substring(pos+1, caption.length()));
-		}	
-		
+		}
+
 		return num;
 	}*/
-	
+
 	//Para obtener los objeto Tkey que contienen una determinada key
 	public Tkey getKey(String key){
 		for (Tkey k : listKey){
@@ -205,60 +205,60 @@ public class I18NConverter {
 		}
 		return null;
 	}
-	
+
 	//Adiciona las llaves creadas
 	private void addKey(String key){
 		if (key.length() > 1){
 			String gKey = generateKey(key);
 			int select = valueAndKeyInList(gKey, key, listKey);
-			
-			
+
+
 			switch (select) {
-				case 1: {					
+				case 1: {
 					Tkey keyAux = getKey(gKey);
-					
+
 					Tkey newKey = new Tkey(gKey,key,javaFileFullClassName,keyAux.maxSuffixClass+1,keyAux.maxSuffixClass+1);
 					updateSuffixMax(gKey,listKey);
-					listKey.add(newKey);					
+					listKey.add(newKey);
 					int a = 9;
-					
+
 				};break;
 				case 2: {
-					
+
 				};break;
 				case 3: {
 					Tkey newKey = new Tkey(gKey,key,javaFileFullClassName,0,0);
-					listKey.add(newKey);					
+					listKey.add(newKey);
 				};break;
-					
+
 			}
 
 		}
-		
-	}	
+
+	}
 
 	//Determina si un texto esta asignado a una variable de tipo String en la clase
-	private boolean isValueInStringValueList(String value){		
+	private boolean isValueInStringValueList(String value){
 		for (TStringValue s : listStringValue){
 			if (s.value.equals(value)){
 				return true;
 			}
 		}
-		return false;		
+		return false;
 	}
-	
+
 	//Determina si una cadena es un ID de variable de tipo String en la clase
-	private boolean isIdInStringValueList(String id){		
+	private boolean isIdInStringValueList(String id){
 		for (TStringValue s : listStringValue){
 			if (s.id.equals(id)){
 				return true;
 			}
 		}
-		return false;		
+		return false;
 	}
 
 	//Obtiene el valor de cada variable de tipo String valida declarada en la clase
-	private String getValueById(String id){		
+	private String getValueById(String id){
 		for (TStringValue s : listStringValue){
 			if (s.id.equals(id)){
 				return s.value;
@@ -266,7 +266,7 @@ public class I18NConverter {
 		}
 		return "";
 	}
-	
+
 	//Almacena todos los valores de las variables de tipo String en cada clase
 	private void addStringVarValue(String id, String value){
 		if (value.length() > 0){
@@ -274,30 +274,30 @@ public class I18NConverter {
 				TStringValue newStringValue = new TStringValue(id,value);
 				listStringValue.add(newStringValue);
 			}
-		
-		}		
+
+		}
 	}
-	
+
 	/*private String getCompositeName(String vaadinName) {
 		if (vaadinName != null) {
 			if (vaadinName.startsWith("com.vaadin.ui.")){
 				return vaadinName;
-			}				
+			}
 		}
 		return null;
 	}*/
-	
+
 	public List<Tkey> getGeneralListKey(){
 		return generalListKey;
 	}
-	
+
 	public List<Tkey> getListKey(){
 		return listKey;
 	}
 
-	
+
 	public void proccessProject(File dirBaseSrc, String path){
-		
+
 		for (File filesrc : dirBaseSrc.listFiles()) {
 			if (filesrc.isDirectory()) {
 				proccessProject(filesrc, path);
@@ -306,13 +306,13 @@ public class I18NConverter {
 
 					if (filesrc.getName().endsWith(".java")) {
 						System.out.println(filesrc.toString());
-						
+
 						listStringValue.clear();
-						
+
 						javaFileName = filesrc.getName().replaceAll(".java", "");
-						
-						proccessJavaFile(filesrc.getAbsolutePath());	
-												
+
+						proccessJavaFile(filesrc.getAbsolutePath());
+
 
 						for (Tkey localkey : listKey){
 							//if (!isInKeyList(localkey.key, generalListKey)){
@@ -320,13 +320,13 @@ public class I18NConverter {
 								//addKeyToGeneralListKey(localkey.key);
 								//generalListKey.add(localkey);
 							//}
-						}						
+						}
 					}
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
-		}		
+		}
 	}
 
 	private String detectDelimiters(String caption){
@@ -353,29 +353,30 @@ public class I18NConverter {
 		if (key.length() > size){
 			return key.substring(0, size);
 		}
-		
+
 		return key;
 	}
-	//Genera llaves de 30 caractéres
+
+	//Genera llaves de 30 caracteres
 	private String generateKey(String caption){
 		String key = detectDelimiters(caption);
-		
+
 		String finalKey = splitKey(key, 30);
-		
+
         if (finalKey.startsWith(".")){
         	finalKey = finalKey.replaceFirst(".", "");
         }
         if (finalKey.endsWith(".")){
         	finalKey = finalKey.substring(0, finalKey.length()-1);
-        } 
-		
+        }
+
 		return finalKey;
-		
+
 	}
-	
+
 	/**
 	 * Entry point to process every class file
-	 * 
+	 *
 	 * @param filename
 	 *            class filename
 	 * @return the converted i18n-aware class
@@ -403,10 +404,10 @@ public class I18NConverter {
 				if (name.startsWith("com.vaadin.ui.")) {
 					lidtarget.add(id);
 				}
-			}		
-		
+			}
+
 		javaFileFullClassName = cutarget.getPackage().getName().toString().replace(".", "_") + "_" + javaFileName + "_";
-		
+
 		List<TypeDeclaration> types = cutarget.getTypes();
 
 		// ahora miramos en cada clase
@@ -417,38 +418,38 @@ public class I18NConverter {
 	}
 
 	private boolean isVaadinComponent(String name){
-		
+
 		for (ImportDeclaration id : lidtarget){
 			if (id.getName().getName().equals(name)){
 				return true;
 			} else if (("I18N"+id.getName().getName()).equals(name)){
 				return true;
 			}
-		}		
-		return false;		
+		}
+		return false;
 	}
-	
+
 	/**
 	 * Process method arguments
-	 * 
+	 *
 	 * @param largs
 	 */
-	
+
 	private String extactExprCaption(ObjectCreationExpr exp){
-		
+
 		Type type = exp.getType();
 			if (isVaadinComponent(type.toString())){
-			
+
 				boolean flag = false;
-				
+
 				try {
 					flag = !exp.getArgs().isEmpty();
 				} catch (Exception e) {
-					// TODO: handle exception					
-				}				
-				
+					// TODO: handle exception
+				}
+
 				if (flag){
-					if (exp.getArgs().get(0) instanceof StringLiteralExpr){						
+					if (exp.getArgs().get(0) instanceof StringLiteralExpr){
 						return ((StringLiteralExpr) exp.getArgs().get(0)).getValue();
 					} else if (isIdInStringValueList(exp.getArgs().get(0).toString())) {
 						addKey(getValueById(exp.getArgs().get(0).toString()));
@@ -457,7 +458,7 @@ public class I18NConverter {
 			}
 		return "";
 	}
-	
+
 	private boolean isValidMethod(String name){
 		boolean is = false;
 		for (int i = 0; i < validMethods.length-1; i++){
@@ -467,9 +468,9 @@ public class I18NConverter {
 		}
 		return is;
 	}
-	
+
 	private void processArgs(List<Expression> largs, String methodName) {
-		if (isValidMethod(methodName)){	
+		if (isValidMethod(methodName)){
 			if (largs != null) {
 				for (int i = 0; i < largs.size(); i++) {
 					if (largs.get(i) instanceof ObjectCreationExpr) {
@@ -478,18 +479,18 @@ public class I18NConverter {
 
 					} else if (largs.get(i) instanceof MethodCallExpr) {
 						processArgs(((MethodCallExpr) largs.get(i)).getArgs(), ((MethodCallExpr) largs.get(i)).getName());
-						
-	
+
+
 					} else if (largs.get(i) instanceof StringLiteralExpr){
-						addKey(((StringLiteralExpr) largs.get(i)).getValue());						
+						addKey(((StringLiteralExpr) largs.get(i)).getValue());
 					} else if (isIdInStringValueList(largs.get(i).toString())){
 						addKey(getValueById(largs.get(i).toString()));
 					}
 				}
 			}
-		} 	
+		}
 	}
-	
+
 	private void processArgs(List<Expression> largs) {
 		if (largs != null) {
 				for (int i = 0; i < largs.size(); i++) {
@@ -499,25 +500,25 @@ public class I18NConverter {
 
 					} else if (largs.get(i) instanceof MethodCallExpr) {
 						processArgs(((MethodCallExpr) largs.get(i)).getArgs(), ((MethodCallExpr) largs.get(i)).getName());
-						
-	
+
+
 					} else if (largs.get(i) instanceof StringLiteralExpr){
-						addKey(((StringLiteralExpr) largs.get(i)).getValue());						
+						addKey(((StringLiteralExpr) largs.get(i)).getValue());
 					}
 				}
-			}	
+			}
 	}
 
 	private String removeQuotation(String param){
-		if (param.length() > 2){			
+		if (param.length() > 2){
 			return param.substring(1, param.length()-1);
 		}
 		return "";
 	}
-	
+
 	/**
 	 * proccess a block statement
-	 * 
+	 *
 	 * @param blockStmt
 	 */
 	void processBlockStmt(BlockStmt blockStmt) {
@@ -530,7 +531,7 @@ public class I18NConverter {
 
 	/**
 	 * Process every expression
-	 * 
+	 *
 	 * @param expression
 	 *            expression
 	 * @return
@@ -538,28 +539,28 @@ public class I18NConverter {
 	private Expression processExpression(Expression expression) {
 		if (expression == null)
 			return null;
-		
+
 		if (expression instanceof AssignExpr) {
 			AssignExpr ae = (AssignExpr) expression;
 			if (ae.getTarget() instanceof NameExpr) {
 				if (ae.getValue() instanceof ObjectCreationExpr) {
-					
+
 					ObjectCreationExpr exp = (ObjectCreationExpr) ae.getValue();
 					addKey(extactExprCaption(exp));
-					
+
 				} else if (ae.getValue() instanceof MethodCallExpr) {
 					processArgs(((MethodCallExpr) ae.getValue()).getArgs(), ((MethodCallExpr) ae.getValue()).getName());
 				}
 			}
 		} else if (expression instanceof VariableDeclarationExpr) {
 			VariableDeclarationExpr vde = (VariableDeclarationExpr) expression;
-			
+
 			if (vde.getType() instanceof ReferenceType) {
 
 				for (VariableDeclarator vd : vde.getVars()) {
 					if (vd.getInit() != null) {
 						if (vd.getInit() instanceof ObjectCreationExpr) {
-							
+
 							ObjectCreationExpr exp = (ObjectCreationExpr) vd.getInit();
 							addKey(extactExprCaption(exp));
 
@@ -568,16 +569,16 @@ public class I18NConverter {
 				}
 			}
 		} else if (expression instanceof MethodCallExpr) {
-		
+
 			processArgs(((MethodCallExpr) expression).getArgs(), ((MethodCallExpr) expression).getName());
 
 		} else if (expression instanceof CastExpr) {
 			CastExpr ce = (CastExpr) expression;
 			if (ce.getExpr() instanceof ObjectCreationExpr) {
-				
+
 				ObjectCreationExpr exp = (ObjectCreationExpr) ce.getExpr();
 				addKey(extactExprCaption(exp));
-				
+
 			} else if (ce.getExpr() instanceof MethodCallExpr) {
 				processArgs(((MethodCallExpr) ce.getExpr()).getArgs(), ((MethodCallExpr) ce.getExpr()).getName());
 			}
@@ -593,10 +594,10 @@ public class I18NConverter {
 					}
 			}
 		} else if (expression instanceof ObjectCreationExpr) {
-			
+
 			ObjectCreationExpr exp = (ObjectCreationExpr) expression;
 			addKey(extactExprCaption(exp));
-			
+
 		} else if (expression instanceof ClassExpr) {
 			ClassExpr ce = (ClassExpr) expression;
 			ReferenceType rt = (ReferenceType) ce.getType();
@@ -618,25 +619,25 @@ public class I18NConverter {
 		}
 		return expression;
 	}
-	
+
 	/**
 	 * Process every member
-	 * 
+	 *
 	 * @param member
 	 *            member to process
 	 */
 	private void processMember(BodyDeclaration member) {
-		if (member instanceof FieldDeclaration){	
-			
+		if (member instanceof FieldDeclaration){
+
 			FieldDeclaration fd = (FieldDeclaration) member;
-			
+
 			if (!(fd.getType() instanceof PrimitiveType)){
 				if (fd.getType().toString().equals("String")){
 					for (VariableDeclarator vd : fd.getVariables()) {
 						if (vd.getInit() != null) {
 							if ((!vd.getInit().toString().contains("+")) & (!vd.getInit().toString().contains("null"))){
 								String v = ((StringLiteralExpr) vd.getInit()).getValue();
-								if (!isValueInStringValueList(v)){									
+								if (!isValueInStringValueList(v)){
 									addStringVarValue(vd.getId().toString(), v);
 								}
 							}
@@ -646,20 +647,20 @@ public class I18NConverter {
 					for (VariableDeclarator vd : fd.getVariables()) {
 						if (vd.getInit() != null) {
 							if (vd.getInit() instanceof ObjectCreationExpr) {
-								
+
 								ObjectCreationExpr exp = (ObjectCreationExpr) vd.getInit();
 								addKey(extactExprCaption(exp));
-		
+
 							}
 						}
 					}
 				}
 			}
-			
+
 		}else if (member instanceof ClassOrInterfaceDeclaration) {
 				for (BodyDeclaration member1 : ((ClassOrInterfaceDeclaration) member).getMembers()) {
 					processMember(member1);
-				}				
+				}
 		} else if (member instanceof MethodDeclaration || member instanceof ConstructorDeclaration) {
 
 			BlockStmt blockStmk;
@@ -676,12 +677,12 @@ public class I18NConverter {
 
 			processBlockStmt(blockStmk);
 
-		} 
+		}
 	}
-	
+
 	/**
 	 * Process every statement. Maybe not all Java statements be supported
-	 * 
+	 *
 	 * @param statement
 	 *            statement to process
 	 */
@@ -689,14 +690,14 @@ public class I18NConverter {
 		if (statement instanceof ExpressionStmt) {
 			ExpressionStmt es = (ExpressionStmt) statement;
 			processExpression(es.getExpression());
-			
+
 		} else if (statement instanceof BlockStmt) {
 			BlockStmt bs = (BlockStmt) statement;
 			processBlockStmt(bs);
 		} else if (statement instanceof SynchronizedStmt) {
 			SynchronizedStmt bs = (SynchronizedStmt) statement;
 			BlockStmt bs1 = (BlockStmt) bs.getBlock();
-			processBlockStmt(bs1);	
+			processBlockStmt(bs1);
 		} else if (statement instanceof SwitchStmt) {
 			SwitchStmt bs = (SwitchStmt) statement;
 			for (SwitchEntryStmt swe : bs.getEntries()) {
@@ -766,7 +767,7 @@ public class I18NConverter {
 
 	/**
 	 * Process every type
-	 * 
+	 *
 	 * @param type
 	 */
 	private void processType(TypeDeclaration type) {
